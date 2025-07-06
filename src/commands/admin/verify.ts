b95616d5882c01306f66d6e2e2288174ee6e72d0
MeltyMooncakes <meltymooncakes@gmail.com>
2025-07-05 20:28:43 -0400

    bwa

--- src/commands/admin/verify.ts
+++ src/commands/admin/verify.ts
@@ -20,33 +20,23 @@ export class Command extends DiscordCommand {
 			return;
 		}
 
-		
-		const channel = await (await this.client.guilds.fetch("1231247596459130974")).channels.fetch("1233485156325658634");
-		
-		if (!channel) {
-			return;
-		}
-
-		if (channel.isDMBased() || !channel.isTextBased()) {
-			return;
-		}
-
 		const row = new ActionRowBuilder()
 			.addComponents(
 				new ButtonBuilder()
 					.setCustomId("girls-verify")
 					.setLabel("Verify")
 					.setStyle(ButtonStyle.Success)
+					.setDisabled(false)
 			);
 
 
-		channel.send({
+		message.channel.send({
 			embeds: [
 				new EmbedBuilder()
 					.setTitle("Verification")
 					.setDescription("Click the button below to verify.").toJSON()
 			],
-			//@ts-ignore
+			// @ts-ignore
 			components: [row]
 		});
 	}
